27694c20e30664066c501c8303dba03e4e7d3cfd
Wenjie <traxiaojie@gmail.com>
2018-04-03 16:04:20 -0700

    Added form for driver and passenger Added validation

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,5 @@
 class Driver < ApplicationRecord
   has_many :trips
+  validates :name, presence: true
+  validates :vin, presence: true 
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   has_many :trips
+  validates :name, presence: true
+  validates :phone_num, presence: true
 end
