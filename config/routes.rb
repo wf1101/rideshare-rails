0f2d88918c1c6ccce726e40ca9eb341e64e9ef43
Dikla <dikla.aharoni@gmail.com>
2018-04-04 12:09:45 -0700

    Added nested routes in passenger and driver

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'drivers#index'
 
-  resources :drivers
-  resources :passengers
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
+  resources :passengers do
+    resources :trips, only: [:index, :create]
+  end
   resources :trips
+
+
 end
